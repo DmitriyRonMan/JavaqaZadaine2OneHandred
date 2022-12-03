b435324d449eb2b255f5e11a464ee41e889e11cd
Dmitriy <dimon-9_93@mail.ru>
2022-12-03 15:12:37 +0500

    README change

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int check = 240;
-        double repAmount = 700.67;
+        double repAmount = 1000.5;
         int bonus;
-        if (repAmount > 100) {
+        if (repAmount >= 1000.5) {
             bonus = (int) repAmount / 100;
         } else {
             bonus = 0;
